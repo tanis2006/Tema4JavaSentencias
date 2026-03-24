91f16bf3480586250d6f45078e5ba86e3f87bf22
mejia <mejia@KENINI>
2026-03-24 13:19:49 -0600

    comit tema 4

---
+++ src/main/java/com/mycompany/tema4sentenciaslogicas/Tema4SentenciasLogicas.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tema4sentenciaslogicas;
+import java.util.Scanner;
+
+/**
+ *
+ * @author mejia
+ */
+public class Tema4SentenciasLogicas {
+
+    public static void main(String[] args) {
+        if (true){
+        System.out.println("Esta linea se ejecuto correctamente");
+    }
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingresa el valor de x ");
+        int x=sc.nextInt();
+        System.out.println("Ingresa el valor de y");
+        int y=sc.nextInt();
+        
+if(x>y) {
+    System.out.println("X es mayor que Y");
+}
+else if (y>x) {
+    System.out.println("Y es mayor que x");
+}
+else{
+    System.out.println("X,Y tienen el mismo valor");
+}
+
+int numero=sc.nextInt();
+if(numero>0) {
+    System.out.println("el numero" + numero+"es positivo");
+}
+else if (numero<0){
+    System.out.println("el numero" + numero +"es negativo");
+}else{
+    System.out.println("El numero es cero");
+}}}
+
+
+
+            
+
+    
+
+        
+
